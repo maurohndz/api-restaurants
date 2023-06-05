f23294321af508683649ab3c489dd29d436c84ed
Mauro Hernandez <mhernandez@prolesys.com>
2023-06-05 14:11:28 -0400

    obtener el puerto de las variables de entorno

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port = process.env['PORT'] || 3000;
 
   app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(new ValidationPipe());
-  
-  await app.listen(3000);
+
+  await app.listen(port).then(() => console.log(`API run in port ${port}`));
 }
 bootstrap();
